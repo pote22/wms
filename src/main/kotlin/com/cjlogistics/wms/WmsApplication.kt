bf97c76a29e61c13cde5017c175cbd13c088cbc2
허민 <pote1471@gmail.com>
2026-07-06 16:32:37 +0900

    jwt 토큰 설정 수정

--- src/main/kotlin/com/cjlogistics/wms/WmsApplication.kt
+++ src/main/kotlin/com/cjlogistics/wms/WmsApplication.kt
@@ -1,9 +1,11 @@
 package com.cjlogistics.wms
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class WmsApplication
 
 fun main(args: Array<String>) {
